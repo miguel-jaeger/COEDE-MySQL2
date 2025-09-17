43f166412d8a7de22762047022f8a05cbf601f6c
mcolquehuancas124 <mcolquehuancas@esge.edu.pe>
2025-09-17 12:29:56 -0500

    cambio

--- MODELO/Conexion.php
+++ MODELO/Conexion.php
@@ -1,15 +1,17 @@
 <?php
 class Conexion {
     private $host = "localhost";
-    private $db_name = "db_calificaciones";
-    private $username = "root";  // por defecto en XAMPP
+    private $db_name = "db_calificaciones"; // 👈 debe coincidir con lo que creaste
+    private $username = "root";  // XAMPP usa root por defecto
     private $password = "";      // vacío por defecto en XAMPP
     public $conn;
 
     public function getConexion() {
         $this->conn = null;
         try {
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, 
+                                  $this->username, 
+                                  $this->password);
             $this->conn->exec("set names utf8");
         } catch (PDOException $exception) {
             echo "Error de conexión: " . $exception->getMessage();
