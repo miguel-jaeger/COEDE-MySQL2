150a9a5ebd0a0328e9c607e5273cf12fa6a743c7
mcolquehuancas124 <mcolquehuancas@esge.edu.pe>
2025-09-17 12:17:02 -0500

    edite conexion.php

--- MODELO/Conexion.php
+++ MODELO/Conexion.php
@@ -1,36 +1,19 @@
 <?php
 class Conexion {
-    public static function conectar() {
-        $host = "localhost";
-        $user = "root";
-        $pass = "";
-        $db = "db_calificaciones";
-
-<<<<<<< HEAD
-class Database {
     private $host = "localhost";
-    //Datos a cambiar
-    private $db_name = "db_ingenieria";
-    private $username = "root";
-    private $password = "";
+    private $db_name = "db_calificaciones";
+    private $username = "root";  // por defecto en XAMPP
+    private $password = "";      // vacío por defecto en XAMPP
     public $conn;
 
-    public function getConnection(){
+    public function getConexion() {
         $this->conn = null;
-        try{
-            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
-            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        }catch(PDOException $exception){
-            echo "Error de conexión: " . $exception->getMessage();
-=======
         try {
-            $conn = new PDO("mysql:host=$host;dbname=$db;charset=utf8", $user, $pass);
-            $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-            return $conn;
-        } catch (PDOException $e) {
-            die("Error de conexión: " . $e->getMessage());
->>>>>>> 79c37edd312b6e0beae3c04f114a96be6baa2306
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn->exec("set names utf8");
+        } catch (PDOException $exception) {
+            echo "Error de conexión: " . $exception->getMessage();
         }
+        return $this->conn;
     }
 }
-?>
