80a18d19dcb909942cfa128c4a068bfbba291a3d
mcolquehuancas124 <mcolquehuancas@esge.edu.pe>
2025-09-17 12:14:58 -0500

    db_calificaciones

--- backup/backup.sql
+++ backup/backup.sql
@@ -1,13 +1,13 @@
-CREATE DATABASE IF NOT EXISTS db_calificaciones
-  DEFAULT CHARACTER SET utf8mb4
-  COLLATE utf8mb4_general_ci;
+-- Crear la base de datos
+CREATE DATABASE db_calificaciones;
 
+-- Usar la base de datos
 USE db_calificaciones;
 
+-- Crear la tabla calificaciones
 CREATE TABLE calificaciones (
-  id INT NOT NULL AUTO_INCREMENT,
-  materia VARCHAR(50) NOT NULL,
-  nota FLOAT NOT NULL,
-  comentario VARCHAR(500) NOT NULL,
-  PRIMARY KEY (id)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    materia VARCHAR(20) NOT NULL,
+    nota FLOAT NOT NULL,
+    comentario VARCHAR(500) NOT NULL
+);
