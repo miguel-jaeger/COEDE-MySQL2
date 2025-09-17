0b7006a8ab629e04b93adb131010670b3d37627e
mcolquehuancas124 <mcolquehuancas@esge.edu.pe>
2025-09-17 08:02:46 -0500

    registro php completo

--- VISTA/Registro.php
+++ VISTA/Registro.php
@@ -6,12 +6,15 @@
 <body>
     <h1>REGISTRO INDIVIDUAL DE NOTAS (RIN) </h1>
     <form action="index.php" method="POST">
-        <label for="nombre">Nombre:</label><br>
+        <label for="nombre">Curso:</label><br>
         <input type="text" id="nombre" name="nombre" required><br>
-        <label for="apellido">Apellido:</label><br>
+        <label for="apellido">Nota:</label><br>
         <input type="text" id="apellido" name="apellido" required><br>
         <label for="correo">Correo:</label><br>
         <input type="email" id="correo" name="correo" required><br><br>
+        <label for="sugerencia">Sugerencia:</label><br>
+        <textarea id="sugerencia" name="sugerencia" type="text" required>
+        </textarea>
         <input type="hidden" name="action" value="crear">
         <button type="submit">Guardar</button>
     </form>
