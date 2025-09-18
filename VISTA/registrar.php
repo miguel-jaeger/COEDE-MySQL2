4b6fbdee660fb579dc2fbfb5196f274672021c4b
killerwade69 <patitiur01@gmail.com>
2025-09-18 01:48:23 -0500

    a

--- VISTA/registrar.php
+++ VISTA/registrar.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Registro de Notas</title>
-    <link rel="stylesheet" href="">
+    <link rel="stylesheet" href="styles.css">
 </head>
 <body>
 <div class="container">
