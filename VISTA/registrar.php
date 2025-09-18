ed63824f3d882579f062b4ce09f2fd8cdfc631b3
mcolquehuancas124 <mcolquehuancas@esge.edu.pe>
2025-09-18 01:04:54 -0500

    EDITE OPCIONES REGISTRAR

--- VISTA/registrar.php
+++ VISTA/registrar.php
@@ -5,8 +5,15 @@
     <label>Nombres:</label>
     <input type="text" name="nombres" required>
 
-    <label>Materia:</label>
-    <input type="text" name="materia" required>
+    <label for="deporte">Materia:</label>
+      <select id="deporte" name="deporte">
+        <option value="" disabled selected>--- Elija un curso ---</option>
+        <option value="0">PHP</option>
+        <option value="1">C++</option>
+        <option value="2">Linux</option>
+        <option value="3">HTML</option>
+        <option value="4">SQL</option>
+      </select>
 
     <label>Nota:</label>
     <input type="number" step="0.01" name="nota" required>
